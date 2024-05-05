40ace0b7ea3b7a479356266f054e899be5e8c4f0
Tapas Jyoti <tapasjyoti8327@gmail.com>
2024-05-05 19:24:41 +0530

    Laying The Foundation

--- app.js
+++ app.js
@@ -28,14 +28,99 @@ import ReactDOM from "react-dom/client";
 
 //-------------------Laying the foundation--------------------
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Namaste React 🚀"
-);
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "Namaste React 🚀"
+// );
 
 //JSX --> It is not HTML in JS(It is a HTML like Syntax)!
-const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>
+
+//JSX => transpiles it to React.createElement => ReactElement-JS Object => HTMLElement(render)
+
+// const jsxHeading = (
+//   <h1 className="head" tabIndex="5">
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+//React Component
+
+// 1-> React Functional Components
+
+// const HeadingComponent = () => {
+//   return <h1 className="heading">Namaste React Functional Components</h1>;
+// };
+
+//Without Using Return Statement
+
+// const Title = () => (
+//   <h1 className="head" tabIndex="5">
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+//Component Compostion
+
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title/>
+//     <h1 className="heading">Namaste React Functional Components 😍</h1>
+//   </div>
+// );
+
+//Putting React element inside React Component
+
+// const title = (
+//   <h1 className="head" tabIndex="5">
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+// const HeadingComponent = () => (
+//   <div id="container">
+//     {title}
+//     <h1 className="heading">Namaste React Functional Components 😍</h1>
+//   </div>
+// );
+
+// Putting React element inside React element
+
+// const ele = <span>React Element</span>;
+
+// const title = (
+//   <h1 className="head" tabIndex="5">
+//     {ele}
+//     Namaste React using JSX 🚀
+//   </h1>
+// );
+
+// const HeadingComponent = () => (
+//   <div id="container">
+//     {title}
+//     <h1 className="heading">Namaste React Functional Components 😍</h1>
+//   </div>
+// );
+
+// Putting React Component inside React element
+
+const ele = <span>React Element</span>;
+
+const HeadingComponent = () => (
+  <h1 className="heading">Namaste React Functional Components 😍</h1>
+);
+
+const title = (
+  <h1 className="head" tabIndex="5">
+    {ele}
+    Namaste React using JSX 🚀
+    <HeadingComponent />
+  </h1>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxHeading);
+// root.render(jsxHeading);
+
+// root.render(<HeadingComponent />);
+
+root.render(title);
